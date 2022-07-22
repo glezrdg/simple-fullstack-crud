7a0539fb564cb9d67c77a0b4a61dd678ff1ec210
German <glezrdg@gmail.com>
2022-07-22 00:59:50 -0400

    first and only commit

---
+++ FBTarjeta/FBTarjeta/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FBTarjeta.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FBTarjeta
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
